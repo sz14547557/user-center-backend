12abe5c322ced06858cbb02a611d26e5d0853ac1
sz <14547557@qq.com>
2024-02-20 10:16:21 +0800

    修改数据库表编码为utf-8

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,12 +1,10 @@
 # 数据库初始化
-# @author <a href="https://github.com/liyupi">程序员鱼皮</a>
-# @from <a href="https://yupi.icu">编程导航知识星球</a>
 
 -- 创建库
-create database if not exists yupi;
+create database if not exists userCenter;
 
 -- 切换库
-use yupi;
+use userCenter;
 
 # 用户表
 create table user
@@ -26,10 +24,8 @@ create table user
     isDelete     tinyint  default 0                 not null comment '是否删除',
     userRole     int      default 0                 not null comment '用户角色 0 - 普通用户 1 - 管理员',
     planetCode   varchar(512)                       null comment '星球编号'
-)
+) CHARACTER SET = utf8
     comment '用户';
 
 # 导入示例用户
-INSERT INTO yupi.user (username, userAccount, avatarUrl, gender, userPassword, phone, email, userStatus, createTime, updateTime, isDelete, userRole, planetCode) VALUES ('鱼皮', 'yupi', 'https://himg.bdimg.com/sys/portraitn/item/public.1.e137c1ac.yS1WqOXfSWEasOYJ2-0pvQ', null, 'b0dd3697a192885d7c055db46155b26a', null, null, 0, '2023-08-06 14:14:22', '2023-08-06 14:39:37', 0, 1, '1');
-
-# [加入编程导航](https://t.zsxq.com/0emozsIJh) 入门捷径+交流答疑+项目实战+求职指导，帮你自学编程不走弯路
+INSERT INTO userCenter.user (username, userAccount, avatarUrl, gender, userPassword, phone, email, userStatus, createTime, updateTime, isDelete, userRole, planetCode) VALUES ('某政', 'mouzheng', 'https://himg.bdimg.com/sys/portraitn/item/public.1.e137c1ac.yS1WqOXfSWEasOYJ2-0pvQ', null, 'b0dd3697a192885d7c055db46155b26a', null, null, 0, '2023-08-06 14:14:22', '2023-08-06 14:39:37', 0, 1, '1');
