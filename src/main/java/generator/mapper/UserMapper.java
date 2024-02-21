5aeba88e7ec811e1f87944134fb170ee0baf8d29
sz <14547557@qq.com>
2024-02-21 10:20:00 +0800

    使用MybatisX-Generator插件快速生成Mybatis基础文件

---
+++ src/main/java/generator/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author sz
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-02-21 10:18:14
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/generator/service/UserService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author sz
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2024-02-21 10:18:14
+*/
+public interface UserService extends IService<User> {
+
+}
